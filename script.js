9cd96f7ea1e40044c4dc5e70410bb0685492d05f
Janmejay Buranpuri <jburanpu@uwo.ca>
2020-10-05 22:59:25 -0400

    Added JS comments

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let pokemon = [["01","Bulbasaur","Grass, Poison"],["02","Ivysaur","Grass, Poison
 ["17","Pidgeotto","Normal, Flying"],["18","Pidgeot","Normal, Flying"],["19","Rattata","Normal"],["20","Raticate","Normal"]] //array of pokemon
 
 
-function stringSearch(){
+function stringSearch(){ //function to search for pokemon names
     var node = document.createElement("h2");
     node.id = "searchHeader";
     node.textContent =  "Search";
@@ -13,23 +13,23 @@ function stringSearch(){
         var search = document.getElementById("search");
         document.body.insertBefore(node, search);
     }
-    var uInput = document.getElementById("nameInput").value.toLowerCase();
+    var uInput = document.getElementById("nameInput").value.toLowerCase(); //takes input and makes it lowercase
     var node= document.getElementById("search");
     node.querySelectorAll('*').forEach(n => n.remove())
-    if(uInput!="" && uInput.length <= 20 && !/[^a-zA-Z]/.test(uInput)){
+    if(uInput!="" && uInput.length <= 20 && !/[^a-zA-Z]/.test(uInput)){ //specifies the input
         for (var i = 0; i < pokemon.length; i++) {
-            if(pokemon[i][1].toLowerCase().includes(uInput)){
+            if(pokemon[i][1].toLowerCase().includes(uInput)){ //goes through the array to find required string
                 createPokemon(i,"search");
             }
         }
     }
-    if (uInput==""){
+    if (uInput==""){ //removes the header when not using search
         header.remove();
     }
 }
 
 
-function numSearch(){
+function numSearch(){ //search function for pokemon number
     var node = document.createElement("h2");
     node.id = "searchHeader";
     node.textContent =  "Search";
@@ -41,43 +41,43 @@ function numSearch(){
     var uInput = document.getElementById("numInput").value.toLowerCase();
     var node= document.getElementById("search");
     node.querySelectorAll('*').forEach(n => n.remove())
-    if(uInput!="" && parseInt(uInput) >= 1 && parseInt(uInput) <= 20){
+    if(uInput!="" && parseInt(uInput) >= 1 && parseInt(uInput) <= 20){ // makes sure input is a number from 1-20
         for (var i = 0; i < pokemon.length; i++) {
             if(pokemon[i][0].includes(uInput)){
-                createPokemon(i,"search");
+                createPokemon(i,"search"); //finds the pokemon and creates a container for it
             }
         }
     }
     else if (uInput==""){
-        header.remove();
+        header.remove(); //removes header when not in use
     }
 }
 
-function createPokemon(x,location){
-    var node = document.createElement("li");
+function createPokemon(x,location){ //function to create pokemon boxes 
+    var node = document.createElement("li"); //to list the pokemon
     node.id = pokemon[x][1];
     node.className ="card";
 
-    var pokeName = document.createElement("p");
+    var pokeName = document.createElement("p"); //to list the pokemon name
     pokeName.textContent = pokemon[x][0] + " - " +pokemon[x][1];
     pokeName.className ="name";
 
-    var image = document.createElement("img"); 
+    var image = document.createElement("img"); //to show pictures
     image.src="pokemon/"+(x+1)+".png";
     image.className = "li img";
 
-    var desc = document.createElement("p");
+    var desc = document.createElement("p"); //list pokemon discription 
     desc.textContent = "Type: "+pokemon[x][2];
     desc.className ="cardType";
 
 
-    document.getElementById(location).appendChild(node);
+    document.getElementById(location).appendChild(node); //shows box in the proper location 
     document.getElementById(pokemon[x][1]).appendChild(pokeName);
     document.getElementById(pokemon[x][1]).appendChild(image);
     document.getElementById(pokemon[x][1]).appendChild(desc);
 }
 
-function createPokemons(){
+function createPokemons(){ //creates each pokemon for the body of the HTML
     for(var i =0; i < 20 ; i++){
         createPokemon(i,"pokedex")
     }
